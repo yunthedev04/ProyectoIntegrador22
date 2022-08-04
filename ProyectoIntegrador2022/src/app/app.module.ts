d70c45f3569c7602a4fb95434f4607e881efdb7f
yunthedev04 <usospotify2022@gmail.com>
2022-08-04 18:20:43 -0300

    4/8 18:20

--- ProyectoIntegrador2022/src/app/app.module.ts
+++ ProyectoIntegrador2022/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedessocialesComponent } from './components/redessociales/redessociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    RedessocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
